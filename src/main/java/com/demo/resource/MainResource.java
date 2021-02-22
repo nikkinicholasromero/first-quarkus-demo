4b6235949eb2b3a11d5679a9526d6ca04c4f55f5
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-02-22 12:20:46 -0500

    Initial commit

---
+++ src/main/java/com/demo/resource/MainResource.java
@@ -0,0 +1,15 @@
+package com.demo.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class MainResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String main() {
+        return "Hello, World";
+    }
+}
